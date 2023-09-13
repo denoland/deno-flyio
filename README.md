cc685669fd0ac7f7746e648230e038b16df377f2
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-09-13 15:56:25 -0500

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Deno KV on fly.io
+
+This example application demonstrates how to run a Deno server on
+[fly.io](https://fly.io) that uses [Deno KV](https://www.deno.com/kv) for
+persistence thanks to
+[KV connect](https://deno.com/deploy/docs/kv#connect-to-managed-databases-from-outside-of-deno-deploy).
+
+## License
+
+MIT
---
+++ server.tsx
@@ -0,0 +1,76 @@
+/** @jsx jsx */
+import { Hono } from "https://deno.land/x/hono@v3.5.6/mod.ts";
+import { jsx } from "https://deno.land/x/hono@v3.5.6/middleware.ts";
+
+interface Todo {
+  text: string;
+}
+
+// Connect to a Deno Deploy KV database
+// const dbId = "7dd0d727-3fb0-4c06-84ff-a7062921f940";
+// const dbUrl = `https://api.deno.com/databases/${dbId}/connect`;
+// const kv = await Deno.openKv(dbUrl);
+
+const kv = await Deno.openKv();
+
+const app = new Hono();
+
+app.get("/", async (c) => {
+  const iter = await kv.list<Todo>({ prefix: ["todos"] });
+  const todos = [];
+  for await (const res of iter) todos.push(res.value);
+
+  return c.html(
+    <html>
+      <head>
+        <title>Deno KV TODOs</title>
+      </head>
+      <body>
+        <main style={{ maxWidth: 600, margin: "0 auto" }}>
+          <h1>TODO List</h1>
+          <form action="/todo" method="POST">
+            <input type="text" name="todo" required></input>
+            <button type="submit" style={{ margin: "5px" }}>
+              Add TODO
+            </button>
+          </form>
+
+          {todos.map((todo) => (
+            <div style={{ margin: "10px 0" }}>
+              <form
+                style={{ display: "inline", marginRight: "10px" }}
+                action="/todo/delete"
+                method="POST"
+              >
+                <input type="hidden" name="todo" value={todo.text}></input>
+                <button type="submit">X</button>
+              </form>
+              <span>{todo.text}</span>
+            </div>
+          ))}
+        </main>
+      </body>
+    </html>,
+  );
+});
+
+app.post("/todo", async (c) => {
+  const body = await c.req.parseBody<{ todo: string }>();
+  const todo: Todo = {
+    text: body.todo,
+  };
+
+  await kv.set(["todos", todo.text], todo);
+
+  return c.redirect("/", 303);
+});
+
+app.post("/todo/delete", async (c) => {
+  const body = await c.req.parseBody<{ todo: string }>();
+
+  await kv.delete(["todos", body.todo]);
+
+  return c.redirect("/", 303);
+});
+
+Deno.serve(app.fetch);
